68398e1a88377d8dd6f16479b121e42563e4b795
ShengzheH <95061266+ShengzheH@users.noreply.github.com>
2022-10-21 16:32:53 +0800

    Update Lab08Application.java

--- src/main/java/se331/rest/Lab08Application.java
+++ src/main/java/se331/rest/Lab08Application.java
@@ -19,8 +19,7 @@ public class Lab08Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("http://52.23.208.232:3000")
+                        .allowedOrigins("http://3.83.254.201:3000","http://3.83.254.201:4000")
                         .exposedHeaders("x-total-count");
 
             }
